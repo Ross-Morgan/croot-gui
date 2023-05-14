39c7c0280fa20d3e640ead57a892335814e202aa
Ross-Morgan <morgan.ross.512@gmail.com>
2023-05-14 23:22:12 +0100

    add prelude and optional features

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[cfg(feature = "gui")]
 pub mod app;
+
 pub mod graph;
 
 pub mod prelude {
+    use super::*;
 
+    pub use graph::generate_graph;
+
+    #[cfg(feature = "gui")]
+    pub use graph::show_graph;
 }
