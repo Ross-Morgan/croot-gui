a28c68b324cc8e9f969f2c9486dfaf88d887a468
Ross Morgan <morgan.ross.512@gmail.com>
2023-05-14 12:52:51 +0100

    intial commit

---
+++ src/graph.rs
@@ -0,0 +1,38 @@
+use num_complex::Complex64;
+use plotters::{prelude::*, coord::Shift};
+
+
+fn plot_root<'a>(chart: &'a mut DrawingArea<BitMapBackend<'a>, Shift>) {
+
+}
+
+pub fn generate_graph(filename: &str, dimensions: (u32, u32)) -> Result<(), Box<dyn std::error::Error>> {
+    let root = BitMapBackend::new(filename, dimensions).into_drawing_area();
+    root.fill(&WHITE)?;
+    let mut chart = ChartBuilder::on(&root)
+        .caption("y=x^2", ("sans-serif", 50).into_font())
+        .margin(5)
+        .x_label_area_size(30)
+        .y_label_area_size(30)
+        .build_cartesian_2d(-1f32..1f32, -0.1f32..1f32)?;
+
+    chart.configure_mesh().draw()?;
+
+    chart
+        .draw_series(LineSeries::new(
+            (-50..=50).map(|x| x as f32 / 50.0).map(|x| (x, x * x)),
+            &RED,
+        ))?;
+        .label("y = x^2")
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+
+    chart
+        .configure_series_labels()
+        .background_style(&WHITE.mix(0.8))
+        .border_style(&BLACK)
+        .draw()?;
+
+    root.present()?;
+
+    Ok(())
+}
